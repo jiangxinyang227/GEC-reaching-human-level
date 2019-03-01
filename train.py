da3359e2c550b03ffcec80356b7e85e0f0eb1fd5
jiangxinyang <334973834@qq.com>
2019-03-01 17:38:09 +0800

    revise config

--- train.py
+++ train.py
@@ -15,27 +15,28 @@ flags.DEFINE_integer("num_layers", 7, "Number of layers in each encoder and deco
 flags.DEFINE_integer("embedding_size", 500, "Embedding dimensions of encoder and decoder inputs")
 flags.DEFINE_integer("kernel_size", 3, "kernel size of conv")
 flags.DEFINE_integer("num_filters", 2048, "filter numbers of conv")
-flags.DEFINE_float("learning_rate", 1e-3, "Learning rate")
-flags.DEFINE_integer("batch_size", 16, "Batch size")
-flags.DEFINE_float("keep_prob", 0.9, "keep dropout prob")
+flags.DEFINE_float("learning_rate", 2e-5, "Learning rate")
+flags.DEFINE_integer("batch_size", 32, "Batch size")
+flags.DEFINE_float("keep_prob", 0.2, "keep dropout prob")
 flags.DEFINE_integer("epochs", 30, "Maximum # of training epochs")
-
-flags.DEFINE_float("ratio", 0.01, "eval data ratio")
 
 flags.DEFINE_integer("steps_per_checkpoint", 100, "Save model checkpoint every this iteration")
 flags.DEFINE_string("model_dir", "model/", "Path to save model checkpoints")
 flags.DEFINE_string("model_name", "gec", "File name used for model checkpoints")
 
-flags.DEFINE_string("source_file", "data/lang-8/lang8-train.en", "source sentence file path")
-flags.DEFINE_string("target_file", "data/lang-8/lang8-train.gec", "target sentence file path")
+flags.DEFINE_string("source_file", "data/token_lang-8/lang8-train.en", "source train file path")
+flags.DEFINE_string("target_file", "data/token_lang-8/lang8-train.gec", "target train file path")
+flags.DEFINE_string("source_valid", "data/token_lang-8/lang8-valid.en", "source valid file path")
+flags.DEFINE_string("target_valid", "data/token_lang-8/lang8-valid.gec", "target valid file path")
 
-
-dataSet = DataSet(FLAGS.embedding_size, FLAGS.source_file, FLAGS.target_file, FLAGS.batch_size,
-                  FLAGS.ratio)
+dataSet = DataSet(FLAGS.embedding_size, FLAGS.source_file, FLAGS.target_file, FLAGS.source_valid,
+                  FLAGS.target_valid, FLAGS.batch_size, is_first=False)
 
 # 生成训练数据和测试数据
-dataSet.gen_train_eval()
+dataSet.gen_train_valid()
 vocab_size = len(dataSet.idx_to_word)
+
+print(vocab_size)
 
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9, allow_growth=True)
 config = tf.ConfigProto(log_device_placement=False, allow_soft_placement=True, gpu_options=gpu_options)
@@ -62,16 +63,18 @@ with tf.device("/device:GPU:0"):
                 print("train: step: {}, loss: {}, f_0.5: {}".format(current_step, loss, f))
                 if current_step % FLAGS.steps_per_checkpoint == 0:
 
-                    eval_losses = []
-                    eval_fs = []
-                    for eval_batch in dataSet.next_batch(dataSet.eval_data):
-                        eval_loss, eval_predictions = model.eval(sess, eval_batch, 1.0)
-                        eval_f = f_beta(eval_predictions.tolist(), eval_batch["targets"], eval_batch["target_length"])
+                    valid_losses = []
+                    valid_fs = []
+                    for valid_batch in dataSet.next_batch(dataSet.valid_data):
+                        valid_loss, valid_predictions = model.valid(sess, valid_batch, 1.0)
+                        valid_f = f_beta(valid_predictions.tolist(), valid_batch["targets"], valid_batch["target_length"])
 
-                        eval_losses.append(eval_loss)
-                        eval_fs.append(eval_f)
-                    print("eval: step: {}, loss: {}, perplexity: {}".format(current_step,
-                                                                            sum(eval_losses) / len(eval_losses),
-                                                                            sum(eval_fs) / len(eval_fs)))
+                        valid_losses.append(valid_loss)
+                        valid_fs.append(valid_f)
+                    print("\n")
+                    print("valid: step: {}, loss: {}, perplexity: {}".format(current_step,
+                                                                             sum(valid_losses) / len(valid_losses),
+                                                                             sum(valid_fs) / len(valid_fs)))
+                    print("\n")
                     checkpoint_path = os.path.join(FLAGS.model_dir, FLAGS.model_name)
                     saver.save(sess, checkpoint_path, global_step=current_step)
